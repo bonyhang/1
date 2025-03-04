5c9383791e0519a2f218ad0aee9d2bb59ea52e41
bonyhang <121714985+bonyhang@users.noreply.github.com>
2025-03-04 11:51:28 +0700

    Create header.php

---
+++ includes/header.php
@@ -0,0 +1,19 @@
+<header>
+    <div class="anchor-ad" id="anchorAd">
+        <script src="ads/ad-script.js"></script> <!-- Load Ad -->
+        <button class="close-ad" onclick="closeAnchorAd()">✖</button>
+    </div>
+
+    <div class="container">
+        <img class="logo" src="images/logo.png" alt="Blog Logo">
+        <nav>
+            <ul>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Tech</a></li>
+                <li><a href="#">Lifestyle</a></li>
+                <li><a href="#">Business</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
